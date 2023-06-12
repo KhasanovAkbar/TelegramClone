e4611180465e69574586b2fbb0e8b95453f2f973
KhasanovAkbar <akbar.hasanov0630@gmail.om>
2023-06-12 12:10:02 +0500

    first commit

---
+++ src/test/java/univ/tuit/telegramclone/TelegramCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package univ.tuit.telegramclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
